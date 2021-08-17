a6e07a5e14b5bb15afbb850b1abfe463f297a4bd
GiovanniSinosini <carvalho.sino@gmail.com>
2021-08-17 15:36:04 +0100

    Inicial structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import 'bootswatch/dist/flatly/bootstrap.css'
+
+class App extends React.Component {
+
+  render(){ 
+      return (
+        <div>
+          <button type="button" className="btn btn-primary">Primary</button>
+        </div>
+      )
+  }
+}
+
+export default App;
