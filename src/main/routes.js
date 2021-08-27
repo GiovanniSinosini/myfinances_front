8bf9126256142f1cdbf9f2dcfeae10ae6360479d
GiovanniSinosini <carvalho.sino@gmail.com>
2021-08-27 22:41:39 +0100

    Created AuthenticatedRoutes

--- src/main/routes.js
+++ src/main/routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Route, Switch, HashRouter } from 'react-router-dom'
+import { Route, Switch, HashRouter, Redirect } from 'react-router-dom'
 
 import Login from '../views/login'
 import UserRegister from '../views/userRegister';
@@ -8,15 +8,36 @@ import Home from '../views/home'
 import ConsultPostings from '../views/postings/consultPostings';
 import RegisterPostings from '../views/postings/registerPosting';
 
+const isUserAuthenticated = () => {
+    return true;
+}
+
+function RoutesAuthenticated( {component: Component, ...props} ){
+    return (
+        <Route {...props} render={ (componentProps) => {
+            if(isUserAuthenticated()){
+                return (
+                    <Component {...componentProps}/>
+                )
+            } else{
+                return(
+                    <Redirect to={ {pathname: '/login', state : { from: componentProps.location}} } />
+                )
+            }
+        }}/>
+    )
+}
+
 function Routes(){
     return(
         <HashRouter>
             <Switch>
-                <Route path="/home" component={Home}/>
                 <Route path="/login" component={Login}/>
                 <Route path="/userRegister" component={UserRegister}/>
-                <Route path="/consultPostings" component={ConsultPostings}/>
-                <Route path="/registerPostings/:id?" component={RegisterPostings}/>
+                
+                <RoutesAuthenticated path="/home" component={Home}/>
+                <RoutesAuthenticated path="/consultPostings" component={ConsultPostings}/>
+                <RoutesAuthenticated path="/registerPostings/:id?" component={RegisterPostings}/>
             </Switch>
         </HashRouter>
     )
